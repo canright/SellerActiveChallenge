b0af8e21f87657cde0b83df76604cbd4820c6cb6
Jim Canright <jim@canright.net>
2018-04-20 16:39:37 -0700

    Initial Challenge

---
+++ Controllers/DiscussionController.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+/*
+using System.Web;
+using System.Net;
+using System.Net.Http;
+*/
+
+namespace SellerActiveChallenge.Controllers
+{
+    [Route("discussion")]
+    public class DiscussionController : Controller
+    {
+        // all routes specified here for easy reference
+        private static class ROUTES
+        {
+            public const String USERS = "users";
+            public const String USER = "users/{id}";
+            public const String USER_EMAIL = "users/email/{email}";
+            public const String POSTS = "posts";
+            public const String POST = "posts/{id}";
+            public const String POSTS_USER = "posts/user/{userId}";
+            public const String POSTS_LATEST = "posts/latest";
+            public const String POSTS_LATEST_USER = "posts/latest/{userId}";
+            public const String COMMENTS = "comments";
+            public const String COMMENT = "comments/{id}";
+            public const String COMMENTS_POST = "comments/post/{postId}";
+        }
+
+        // Page size for "show latest"
+        private const int PAGESIZE = 10;
+
+        [HttpGet("routes")]
+        public String ListRoutes()
+        {
+            return "<a href='/discussion/users'>Users</a><br>\n<a href='/discussion/posts'>Posts</a><br>\n";
+        }
+
+        [HttpGet(ROUTES.USERS)]
+        public IEnumerable<Object> ListAllUsers()
+        {
+            try
+            {
+                return Model.GetTableForEntity(Model.USERS);
+            }
+            catch (Exception e)
+            {
+                return (IEnumerable<Object>)NotFoundHandler(e);
+            }
+        }
+
+        [HttpGet(ROUTES.USER)]
+        public Object FetchUserById(int id)
+        {
+            try
+            {
+                return Model.GetRecordById(Model.USERS, id);
+            }
+            catch (Exception e)
+            {
+                return NotFoundHandler(e);
+            }
+        }
+
+        [HttpGet(ROUTES.USER_EMAIL)]
+        public Object FetchUserByEmail(string email)
+        {
+            try
+            {
+                return Model.GetRecordByFieldValue(Model.USERS, Model.EMAIL, email);
+            }
+            catch (Exception e)
+            {
+                return NotFoundHandler(e);
+            }
+        }
+
+        [HttpGet(ROUTES.POSTS)]
+        public IEnumerable<Object> ListAllPosts()
+        {
+            try
+            {
+                return Model.GetTableForEntity(Model.POSTS);
+            }
+            catch (Exception e)
+            {
+                return (IEnumerable<Object>)NotFoundHandler(e);
+            }
+        }
+
+        [HttpGet(ROUTES.POST)]
+        public Object FetchPostById(int id)
+        {
+            try
+            {
+                return Model.GetRecordById(Model.POSTS, id);
+            }
+            catch (Exception e)
+            {
+                return NotFoundHandler(e);
+            }
+        }
+
+        [HttpGet(ROUTES.POSTS_USER)]
+        public IEnumerable<Object> ListPostsForUserId(int userId)
+        {
+            try
+            {
+                return Model.GetTableByFieldValue(Model.POSTS, Model.USERID, userId.ToString());
+            }
+            catch (Exception e)
+            {
+                return (IEnumerable<Object>)NotFoundHandler(e);
+            }
+        }
+
+        [HttpGet(ROUTES.POSTS_LATEST)]
+        public IEnumerable<Object> ListLatestPosts()
+        {
+            try
+            {
+                return Model.GetLatest(Model.POSTS, PAGESIZE);
+            }
+            catch (Exception e)
+            {
+                return (IEnumerable<Object>)NotFoundHandler(e);
+            }
+        }
+
+        [HttpGet(ROUTES.POSTS_LATEST_USER)]
+        public IEnumerable<Object> ListLatestPostsForUserId(int userId)
+        {
+            try
+            {
+                return Model.GetLatestByFieldValue(Model.POSTS, Model.USERID, userId.ToString(), PAGESIZE);
+            }
+            catch (Exception e)
+            {
+                return (IEnumerable<Object>)NotFoundHandler(e);
+            }
+        }
+
+        [HttpGet(ROUTES.COMMENTS)]
+        public IEnumerable<Object> ListAllComments()
+        {
+            try
+            {
+                return Model.GetTableForEntity(Model.COMMENTS);
+            }
+            catch (Exception e)
+            {
+                return (IEnumerable<Object>)NotFoundHandler(e);
+            }
+        }
+
+        [HttpGet(ROUTES.COMMENT)]
+        public Object FetchCommentById(int id)
+        {
+            try
+            {
+                return Model.GetRecordById(Model.COMMENTS, id);
+            }
+            catch (Exception e)
+            {
+                return NotFoundHandler(e);
+            }
+        }
+
+        [HttpGet(ROUTES.COMMENTS_POST)]
+        public IEnumerable<Object> ListCommentsForPostId(int postId)
+        {
+            try
+            {
+                return Model.GetTableByFieldValue(Model.COMMENTS, Model.POSTID, postId.ToString());
+            }
+            catch (Exception e)
+            {
+                return (IEnumerable<Object>)NotFoundHandler(e);
+            }
+        }
+
+        private NotFoundResult NotFoundHandler(Exception e)
+        {
+            if (e.Message.Contains("404") || e.Message.Contains("Not Found"))
+                return NotFound();
+            else
+                throw e;
+        }
+    }
+}
---
+++ Models/DiscussionModel.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.IO;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace SellerActiveChallenge.Controllers
+{
+   public class Model
+    {
+        public const string ID = "id";
+        public const string USERS = "Users";
+        public const string POSTS = "Posts";
+        public const string COMMENTS = "Comments";
+        public const string USERID = "userId";
+        public const string EMAIL = "email";
+        public const string POSTID = "postId";
+
+        // base web request - returns data response to requested url.
+        static private String GetDataFromUrl(string url)
+        {
+            WebResponse response = WebRequest.Create(url).GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string data = reader.ReadToEnd();
+            reader.Close();
+            response.Close();
+            return data;
+        }
+
+        // queries are rooted at this sample data
+        static private string rootUrl = "http://jsonplaceholder.typicode.com/";
+
+        // executes rooted query and returns string response
+        static private String GetDataFromQuery(string query)
+        {
+            return Model.GetDataFromUrl(Model.rootUrl + query);
+        }
+
+        // executes query and returns record from response
+        static private Object GetRecordFromQuery(string query)
+        {
+            String data = GetDataFromQuery(query);
+            return JsonConvert.DeserializeObject<Object>(data);
+        }
+
+        // executes query and returns array of records from response
+        static private Object[] GetTableFromQuery(string query)
+        {
+            String data = GetDataFromQuery(query);
+            return JsonConvert.DeserializeObject<Object[]>(data);
+        }
+
+        // get array of records for an entity
+        static public Object[] GetTableForEntity(string entity)
+        {
+            return GetTableFromQuery(entity);
+        }
+
+        // get array of latest records for an entity
+        static public Object[] GetLatest(string entity, int pageSize)
+        {
+            Object[] table = GetTableForEntity(entity);
+            return SpliceLatest(table, pageSize);
+        }
+
+        // gets identified record of entity
+        static public Object GetRecordById(string entity, int id)
+        {
+            string query = entity + "/" + id.ToString();
+            return GetRecordFromQuery(query);
+        }
+
+        // gets entity record by a field value
+        static public Object GetRecordByFieldValue(string entity, string field, string value)
+        {
+            string query = entity + "?" + field + "=" + value;
+            return GetTableFromQuery(query)[0];
+        }
+
+        // gets array of entity records by a field value
+        static public Object[] GetTableByFieldValue(string entity, string field, string value)
+        {
+            string query = entity + "?" + field + "=" + value;
+            return GetTableFromQuery(query);
+        }
+
+        // gets array of latest records entity records by a field value
+        static public Object[] GetLatestByFieldValue(string entity, string field, string value, int pageSize)
+        {
+            Object[] table = GetTableByFieldValue(entity, field, value);
+            return SpliceLatest(table, pageSize);
+        }
+
+        // returns last (pageSize) records from the sourc table
+        // the last records are the latest
+        static public Object[] SpliceLatest(Object[] source, int pageSize)
+        {
+            int len = (source.Length >= pageSize) ? pageSize : source.Length;
+            Object[] target = Array.ConvertAll(new Array[len], item => (Object)item);
+            Array.Copy(source, source.Length - len, target, 0, len);
+            return target;
+        }
+    }
+}
